84e547bdd330fb3dad5b6a247dc3e4fefe01e2e1
Luis <santiagomorad@gmail.com>
2022-03-12 15:29:13 -0300

    fixing .gitignore, incluyendo /utils/api.jsx en seguimiento de version

---
+++ src/utils/api.jsx
@@ -0,0 +1,95 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.NODE_ENV === 'production'
+    ? 'https://mutz-hub-back.herokuapp.com'
+    : 'http://127.0.0.1:8000';
+
+export const RESOURCE_URL = process.env.NODE_ENV === 'production'
+    ? 'https://mutz-hub.herokuapp.com'
+    : 'http://127.0.0.1:3000';
+
+const API_BASE = `${BASE_URL}/api`;
+
+export const GET = (
+    options
+) => {
+    return axios({
+        method:'get',
+        timeout: 10000,
+        baseURL: API_BASE,
+        crossDomain:true,
+        withCredentials:true,
+        headers:{
+            'Content-type':'application/json',
+            'Accept':'application/json'
+        },
+        ...options
+    });
+}
+
+export const POST = (
+    options
+) => {
+    return axios({
+        method: 'post',
+        timeout: 8000,
+        baseURL: API_BASE,
+        crossDomain:true,
+        withCredentials:true,
+        headers:{
+            'Content-type':'application/json',
+            'Accept':'application/json'
+        },
+        ...options
+    });
+}
+
+export const DELETE = (
+    options
+) => axios({
+        method: 'delete',
+        url: options.endpoint,
+        timeout: 1000,
+        withCredentials:true,
+        baseURL: API_BASE,
+        headers:{
+            'Content-type':'application/json',
+            'Accept':'application/json'
+        },
+    });
+
+export const PUT = (
+    options
+) => {
+
+    return axios({
+        method: 'put',
+        timeout: 5000,
+        baseURL: API_BASE,
+        crossDomain:true,
+        withCredentials:true,
+        headers:{
+            'Content-type':'application/json',
+            'Accept':'application/json'
+        },
+        ...options
+    });
+}
+
+axios.interceptors.request.use(
+    function (config) {
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    function (config) {
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
